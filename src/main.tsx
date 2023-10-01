ac8c6d4f93f21cd6bca0982e98cbf890c389dd7f
黄振东 <2443717628@qq.com>
2023-10-01 14:07:22 +0800

    feat: 项目具体搭建1

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import "@/styles/reset.less";
+import "@/assets/iconfont/iconfont.less";
+import "@/assets/fonts/font.less";
+import "@/styles/common.less";
+import "@/language/index";
+import "virtual:svg-icons-register";
+import { Provider } from "react-redux";
+import { store, persistor } from "@/redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// react 18 创建（会导致 antd 菜单折叠时闪烁，等待官方修复）
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<Provider store={store}>
+		<PersistGate persistor={persistor}>
+			<App />
+		</PersistGate>
+	</Provider>
+);
