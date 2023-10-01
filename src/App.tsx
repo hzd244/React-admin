39181eec6294829806dd08065d153639950b3832
黄振东 <2443717628@qq.com>
2023-10-01 15:27:42 +0800

    feat:项目具体的搭建2

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import zhCN from "antd/lib/locale/zh_CN";
+import enUS from "antd/lib/locale/en_US";
+import i18n from "i18next";
+import "moment/dist/locale/zh-cn";
+import { HashRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = (props: any) => {
+	const { language, assemblySize, themeConfig, setLanguage } = props;
+	const [i18nLocale, setI18nLocale] = useState(zhCN);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+	return (
+		<HashRouter>
+			<ConfigProvider locale={i18nLocale} componentSize={assemblySize}>
+			</ConfigProvider>
+		</HashRouter>
+	)
 }
 
 export default App;
